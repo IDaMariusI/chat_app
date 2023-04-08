57a24f537ca881c63670e2f65af161dfdab63e53
IDaMariusI <fcovillanueva1988@gmail.com>
2023-04-08 14:33:17 -0600

    63. ChatPage - Inicio

--- lib/src/screens/chat_screen.dart
+++ lib/src/screens/chat_screen.dart
@@ -5,9 +5,44 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ChatScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 1,
+        title: Column(
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14,
+              child: const Text('Te', style: TextStyle(fontSize: 12)),
+            ),
+            const SizedBox(height: 3),
+            const Text(
+              'Alicia Ortega',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                reverse: true,
+                itemBuilder: (_, i) => Text('$i'),
+              ),
+            ),
+            const Divider(height: 1),
+            // TODO: Caja de Texto
+            Container(
+              color: Colors.white,
+              height: 100,
+            ),
+          ],
+        ),
       ),
     );
   }
