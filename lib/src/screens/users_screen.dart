8ad4e0160b9de151781c709c12afa8728f1759f2
IDaMariusI <fcovillanueva1988@gmail.com>
2023-04-08 13:54:03 -0600

    61. Continuacion de nuestra aplicacion

--- lib/src/screens/users_screen.dart
+++ lib/src/screens/users_screen.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
 
-class UsersScreen extends StatelessWidget {
+import 'package:chatapp/src/models/user.dart';
+
+class UsersScreen extends StatefulWidget {
   const UsersScreen({super.key});
 
   @override
+  State<UsersScreen> createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  final users = [
+    User(online: true, email: 'test1@test.com', name: 'María', uid: '1'),
+    User(online: false, email: 'test2@test.com', name: 'Melissa', uid: '2'),
+    User(online: true, email: 'test3@test.com', name: 'Martín', uid: '3'),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('UsersScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        title: const Text('Mi Nombre', style: TextStyle(color: Colors.black54)),
+        leading: IconButton(
+          icon: const Icon(Icons.exit_to_app, color: Colors.black54),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: Icon(Icons.check_circle, color: Colors.blue[400]),
+            // child: Icon(Icons.offline_bolt, color: Colors.red),
+          ),
+        ],
+      ),
+      body: ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (_, i) => ListTile(
+          title: Text(users[i].name),
+          leading: CircleAvatar(child: Text(users[i].name.substring(0, 2))),
+          trailing: Container(
+            height: 10,
+            width: 10,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: users[i].online ? Colors.green[300] : Colors.red),
+          ),
+        ),
+        separatorBuilder: (_, i) => const Divider(),
+        itemCount: users.length,
       ),
     );
   }
